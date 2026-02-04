f00403529b35cf8245db911181ecdcdcf6fd5c1e
Marco Fernandez <mf.marcofernandez@gmail.com>
2026-02-03 23:16:29 -0400

    Initial commit to GitHub

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  dialect: "turso",
+  dbCredentials: {
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN,
+  },
+});
